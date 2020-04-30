2c0c21742bf6de76677f4e9b5744b794137ac87a
XavierReese <xtreese24@gmail.com>
2020-04-30 16:59:55 -0400

    Updates

---
+++ precache-manifest.38cc76bcbe4a3fb86850e68a48ad9084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2ed372c145b2f752b6a63ec19d4335",
+    "url": "/raidercrate/index.html"
+  },
+  {
+    "revision": "0496c76cf4e613c5f865",
+    "url": "/raidercrate/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6df70f0c50394a9c91b9",
+    "url": "/raidercrate/static/js/2.29267bcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/raidercrate/static/js/2.29267bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0496c76cf4e613c5f865",
+    "url": "/raidercrate/static/js/main.b34ad571.chunk.js"
+  },
+  {
+    "revision": "70b701b2605cdd56c7ed",
+    "url": "/raidercrate/static/js/runtime-main.c53c2178.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/raidercrate/static/media/logo.5d5d9eef.svg"
+  }
+]);
